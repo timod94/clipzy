1ebf7ac1a3cb4e11a8ca621cea5da3ff5dd44aeb
timod94 <dorstt94@gmail.com>
2025-03-12 18:43:27 +0100

    removed testing

--- frontend/src/__tests__/App.test.js
+++
@@ -1,6 +0,0 @@
-import { screen } from '@testing-library/react';
-
-
-  
-  // Teste, ob bestimmte Elemente gerendert werden
-  expect(screen.getByText(/Upload/i)).toBeInTheDocument(); // Beispiel: Überprüfe, ob der Link zu "Upload" angezeigt wird
