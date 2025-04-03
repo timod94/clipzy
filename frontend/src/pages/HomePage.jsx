7fb6b91ab75d143a61b3645debc81da96bc0361b
timod94 <dorstt94@gmail.com>
2025-04-03 16:12:16 +0200

    responsive design update

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import '../App.css';
 
 const HomePage = () => {
   return (
-    <div className='homepage'>
-    <h1>Welcome to Clipzy</h1>
-    <h3>The secure and user-friendly video-sharing platform. Upload, manage, and share your videos with complete privacy and control.</h3>
-  </div>
+    <div 
+      className='homepage' 
+      style={{ 
+        marginTop: 'clamp(30px, 8vh, 100px)',
+        padding: '0 20px',
+        textAlign: 'center'
+      }}
+    >
+      <h1>Welcome to Clipzy</h1>
+      <h3>The secure and user-friendly video-sharing platform.</h3> 
+      <h4>Upload, manage, and share your videos with complete privacy and control.</h4>
+    </div>
   );
 };
 
